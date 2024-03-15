9539f8ad0857e473852d1ff14fd612c147b0503d
Karen Reynolds <k.reynolds102@gmail.com>
2024-03-15 16:55:54 -0600

    renaming/restructuring

--- src/main/java/com/everyonewatcheswomenssports/demo/SportsScraper.java
+++ src/main/java/com/everyonewatcheswomenssports/demo/SportsScraper.java
@@ -1,13 +1,4 @@
 package com.everyonewatcheswomenssports.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SportsScraper {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SportsScraper.class, args);
-	}
-
 }
