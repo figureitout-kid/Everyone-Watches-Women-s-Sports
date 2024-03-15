a7b7e52edbdcd57e29781eda1178d149a061cef9
Karen Reynolds <k.reynolds102@gmail.com>
2024-03-15 16:44:02 -0600

    renaming/restructuring main classes

---
+++ src/main/java/com/everyonewatcheswomenssports/demo/App.java
@@ -0,0 +1,4 @@
+package com.everyonewatcheswomenssports.demo;
+
+public class App {
+}
--- src/main/java/com/everyonewatcheswomenssports/demo/SportsScraper.java
+++ src/main/java/com/everyonewatcheswomenssports/demo/SportsScraper.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SportsScraper {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SportsScraper.class, args);
 	}
 
 }
