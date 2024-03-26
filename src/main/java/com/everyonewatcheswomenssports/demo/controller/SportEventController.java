42d0d9f65fc5d7b13030aee0ee53ca63a728a4aa
Karen Reynolds <k.reynolds102@gmail.com>
2024-03-25 19:04:13 -0600

    created Service layer

--- src/main/java/com/everyonewatcheswomenssports/demo/controller/SportEventController.java
+++ src/main/java/com/everyonewatcheswomenssports/demo/controller/SportEventController.java
@@ -1,4 +1,34 @@
 package com.everyonewatcheswomenssports.demo.controller;
 
+import com.everyonewatcheswomenssports.demo.model.SportEvent;
+import com.everyonewatcheswomenssports.demo.service.ScraperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
 public class SportEventController {
+    private final ScraperService scraperService;
+
+    @Autowired
+    public SportEventController(ScraperService scraperService) {
+        this.scraperService = scraperService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SportEvent>> getAllEvents() {
+        List<SportEvent> events = scraperService.getSportsEvents();
+
+        if (events != null && !events.isEmpty()) {
+            return ResponseEntity.ok(events);
+        }
+        else {
+            return ResponseEntity.noContent().build();
+        }
+    }
 }
