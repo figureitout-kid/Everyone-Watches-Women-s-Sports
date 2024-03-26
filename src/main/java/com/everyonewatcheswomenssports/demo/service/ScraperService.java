9affaa85b6881fa6620bf5f44e4133e96b1f1099
Karen Reynolds <k.reynolds102@gmail.com>
2024-03-25 18:16:38 -0600

    created scraperService implementation to get events

--- src/main/java/com/everyonewatcheswomenssports/demo/service/ScraperService.java
+++ src/main/java/com/everyonewatcheswomenssports/demo/service/ScraperService.java
@@ -1,4 +1,26 @@
 package com.everyonewatcheswomenssports.demo.service;
 
+import com.everyonewatcheswomenssports.demo.model.SportEvent;
+import com.everyonewatcheswomenssports.demo.scraper.SportsScraper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ScraperService {
+    private final SportsScraper sportsScraper;
+
+    public ScraperService(SportsScraper sportsScraper) {
+        this.sportsScraper = sportsScraper;
+    }
+
+    public List<SportEvent> getSportsEvents() {
+        try {
+            return sportsScraper.scrapeSportsEvents("https://");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
