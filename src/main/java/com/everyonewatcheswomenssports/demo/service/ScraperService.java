c70bb7882476635a3cd33cdd16a7786bd1f0dd0e
Karen Reynolds <k.reynolds102@gmail.com>
2024-04-03 16:07:04 -0600

    added switch statement to accomodate for future different types of events to scrape

--- src/main/java/com/everyonewatcheswomenssports/demo/service/ScraperService.java
+++ src/main/java/com/everyonewatcheswomenssports/demo/service/ScraperService.java
@@ -2,25 +2,29 @@ package com.everyonewatcheswomenssports.demo.service;
 
 import com.everyonewatcheswomenssports.demo.model.SportEvent;
 import com.everyonewatcheswomenssports.demo.scraper.BaseSportsScraper;
+import com.everyonewatcheswomenssports.demo.scraper.NcaaWomensBasketballTournamentScraper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ScraperService {
-    private final BaseSportsScraper baseSportsScraper;
+    private final NcaaWomensBasketballTournamentScraper ncaaScraper;
 
-    public ScraperService(BaseSportsScraper baseSportsScraper) {
-        this.baseSportsScraper = baseSportsScraper;
+    @Autowired
+    public ScraperService(NcaaWomensBasketballTournamentScraper ncaaScraper) {
+        this.ncaaScraper = ncaaScraper;
     }
 
-    public List<SportEvent> getSportsEvents() {
-        try {
-            return baseSportsScraper.scrapeSportsEvents("https://example.com");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    public List<SportEvent> scrapeEvents(String eventType) {
+        switch (eventType) {
+
+            case "NCAA_WOMENS_BASKETBALL" :
+                return ncaaScraper.scrapeSportsEvents();
+
+            default:
+                throw new IllegalArgumentException("Unsupported event type: " + eventType);
         }
     }
 }
