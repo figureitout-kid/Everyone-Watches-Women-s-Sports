8d91588e72c79279a8cc4722cf77162841eb5ea5
Karen Reynolds <k.reynolds102@gmail.com>
2024-04-04 18:03:01 -0600

    added switch statement for cuBasketball use

--- src/main/java/com/everyonewatcheswomenssports/demo/service/ScraperService.java
+++ src/main/java/com/everyonewatcheswomenssports/demo/service/ScraperService.java
@@ -1,7 +1,7 @@
 package com.everyonewatcheswomenssports.demo.service;
 
 import com.everyonewatcheswomenssports.demo.model.SportEvent;
-import com.everyonewatcheswomenssports.demo.scraper.BaseSportsScraper;
+import com.everyonewatcheswomenssports.demo.scraper.CUWomensBasketballScraper;
 import com.everyonewatcheswomenssports.demo.scraper.NcaaWomensBasketballTournamentScraper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +11,12 @@ import java.util.List;
 @Service
 public class ScraperService {
     private final NcaaWomensBasketballTournamentScraper ncaaScraper;
+    private final CUWomensBasketballScraper cuScraper;
 
     @Autowired
-    public ScraperService(NcaaWomensBasketballTournamentScraper ncaaScraper) {
+    public ScraperService(NcaaWomensBasketballTournamentScraper ncaaScraper, CUWomensBasketballScraper cuScraper) {
         this.ncaaScraper = ncaaScraper;
+        this.cuScraper = cuScraper;
     }
 
     public List<SportEvent> scrapeEvents(String eventType) {
@@ -22,6 +24,9 @@ public class ScraperService {
 
             case "NCAA_WOMENS_BASKETBALL" :
                 return ncaaScraper.scrapeSportsEvents();
+
+            case "CU_WOMENS_BASKETBALL " :
+                return cuScraper.scrapeSportsEvents();
 
             default:
                 throw new IllegalArgumentException("Unsupported event type: " + eventType);
